e0c0daeb005ef8a6757554b4a74a8b6f88645294
github username <github email address>
2019-12-02 15:47:28 -0700

    work heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_184051) do
+ActiveRecord::Schema.define(version: 2019_11_22_221310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,13 +74,12 @@ ActiveRecord::Schema.define(version: 2019_11_26_184051) do
     t.string "last_name"
     t.string "email"
     t.string "phone_number"
-    t.string "username", null: false
-    t.string "password_digest", null: false
+    t.string "username"
+    t.string "password_digest"
     t.string "avatar"
-    t.boolean "admin", default: false
+    t.boolean "admin"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "role"
   end
 
   add_foreign_key "friends", "users"
